259813d3276cc7df337b05fcdf59b8305aea6c27
Carlos Montezano <cmontezanol@gmail.com>
2015-05-06 11:33:33 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+// autenticação feita aqui 
+// redireciona pra $urlIndex
+
+$urlIndex = 'scripts/';
+
+header('Location:' . $urlIndex);
---
+++ scripts/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// a intenção é esse arquivo funcionar como um frontController
+// recebendo todas as requisições e enviando pra quem é responsável por elas
+
+echo '<pre>';
+print_r($_SERVER);
+die;
